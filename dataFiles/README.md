74257564305bf0cf003f61391845faf45d58947b
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-03-04 19:55:55 +0000

    Added Provider Directory and Placeholder Code Files

---
+++ dataFiles/README.md
@@ -0,0 +1,5 @@
+# Data Files
+
+The following Data files are stored in this directory
+
+- `authLibProfiles.db` - a SQLite DB Files to store interim authLib Data.
---
+++ providers/authLib/authLib.py
@@ -0,0 +1,19 @@
+'''
+Test Code - Will be changed later
+'''
+import sqlite3 as sql
+
+db = sql.connect("dataFiles/authLibProfiles.db")
+
+cursor = db.cursor()
+
+cursor.execute("CREATE TABLE authProfilesTest (id TEXT, name TEXT)")
+cursor.execute("INSERT INTO authProfilesTest VALUES  ('1', 'T')")
+cursor.execute("INSERT INTO authProfilesTest VALUES  ('2', 'S')")
+
+db.commit()
+
+for row in cursor.execute('SELECT * FROM authProfilesTest'):
+    print(row)
+
+db.close()
