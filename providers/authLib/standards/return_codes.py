4993d481694aa0084dad8465d5cfd3ca9b8404c9
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-03-27 18:58:32 +0000

    Updated Standardized Return Codes with details, scope and  status #40

--- providers/authLib/standards/return_codes.py
+++ providers/authLib/standards/return_codes.py
@@ -1,2 +1,42 @@
+# Return Codes
 class RETURN_CODES:
-    X001 = "Return"
+    ALR01 = {
+        "scope": "authlib.requests.register",
+        "details": "Invalid Email Address",
+        "response": "fail"
+    }
+    ALR02 = {
+        "scope": "authlib.requests.register",
+        "details": "Request Registered Successfully. OTP Generated.",
+        "response": "success"
+    }
+    ALR03 = {
+        "scope": "authlib.requests.register",
+        "details": "Invalid Email Address",
+        "response": "Failed"
+    }
+    ALR11 = {
+        "scope": "authlib.requests.verify",
+        "details": "Mail Doaesn't Exist in AuthLib. Re-Register with details.",
+        "response": "Failed"
+    }
+    ALR12 = {
+        "scope": "authlib.requests.verify",
+        "details": "Authentication Request Expired. OTP Re-generated.",
+        "response": "Failed"
+    }
+    ALR13 = {
+        "scope": "authlib.requests.verify",
+        "details": "Incorrect OTP. Authentication Failed.",
+        "response": "Failed"
+    }
+    ALR14 = {
+        "scope": "authlib.requests.verify",
+        "details": "Correct OTP. Authentication Successful.",
+        "response": "Success"
+    }
+    ALR15 = {
+        "scope": "authlib.requests.verify",
+        "details": "OTP Expired. Authentication Failed.",
+        "response": "Failed"
+    }
