74b9e341f52ff625665ab90ed1d8225481095349
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-03-29 20:31:06 +0000

    Added Mailer - Mailing System #51

---
+++ providers/mailer/standards/return_codes.py
@@ -0,0 +1,16 @@
+class RETURN_CODES:
+
+    MAIL01 = {
+        "return_code": "MAIL01",
+        "scope": "mailer.send.mail",
+        "details": "Mail Sent!",
+        "response": "success"
+    }
+
+    # Failure Case - Yet to be Implemented
+    MAIL02 = {
+        "return_code": "MAIL02",
+        "scope": "mailer.send.mail",
+        "details": "Could not send mail.",
+        "response": "failure"
+    }
