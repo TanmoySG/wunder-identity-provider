0ff58e76e1b4c044ffb776d2c3c4406273396a78
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-04-12 16:39:55 +0000

    Refactoring Changes

--- providers/loginProvider/handlers/payload.py
+++
@@ -1,9 +0,0 @@
-class PAYLOAD:
-
-    def get(self, profile) -> dict:
-        self.payload = {
-            "usr": profile["email"],
-            "uid": profile["user_uID"],
-            "aat": profile["admin_access_token"]
-        }
-        return self.payload
